9303cda1d3dc9bdb0936cc94dac0056412b45aac
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-08-13 09:05:48 +0100

    Seed data into DB

--- Project2_NZWalks.API/Data/NZWalksDbContext.cs
+++ Project2_NZWalks.API/Data/NZWalksDbContext.cs
@@ -12,5 +12,52 @@ namespace Project2_NZWalks.API.Data
         public DbSet<Walk> Walks { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Difficulty> Difficulties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Create a list of difficulties: Easy,Medium,Hard
+            var difficulties = new List<Difficulty>(){
+                new Difficulty()
+                {
+                    Id= Guid.NewGuid(),
+                    Name = "Easy"
+                },
+                new Difficulty()
+                {
+                    Id= Guid.NewGuid(),
+                    Name = "Medium"
+                },
+                new Difficulty()
+                {
+                    Id= Guid.NewGuid(),
+                    Name = "Hard"
+                }
+            };
+
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+            // Create list of Regions
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id= Guid.NewGuid(),
+                    Code= "LA",
+                    Name="Lagos",
+                    RegionImageURL="https://lagos.jpeg"
+                },
+                new Region()
+                {
+                    Id= Guid.NewGuid(),
+                    Code= "KW",
+                    Name="Kwara",
+                    RegionImageURL="https://kwara.jpeg"
+                }
+            };
+
+            modelBuilder.Entity<Region>().HasData(regions);
+        }
     }
 }
---
+++ Project2_NZWalks.API/Migrations/20240813080359_Seeding data into DB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Project2_NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingdataintoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("5da9fbdb-6205-4895-b796-eea51ade662d"), "Easy" },
+                    { new Guid("be7d05a5-0ba6-472f-8157-9af1070ba722"), "Hard" },
+                    { new Guid("ed90a2b2-2b5c-4cec-9835-0fc61ebe9cc3"), "Medium" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageURL" },
+                values: new object[,]
+                {
+                    { new Guid("1d0fe942-3a4c-44f8-b12e-be421e533b5f"), "LA", "Lagos", "https://lagos.jpeg" },
+                    { new Guid("1ea9f4f7-a277-4255-a812-80d54b6de6ed"), "KW", "Kwara", "https://kwara.jpeg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("5da9fbdb-6205-4895-b796-eea51ade662d"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("be7d05a5-0ba6-472f-8157-9af1070ba722"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("ed90a2b2-2b5c-4cec-9835-0fc61ebe9cc3"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("1d0fe942-3a4c-44f8-b12e-be421e533b5f"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("1ea9f4f7-a277-4255-a812-80d54b6de6ed"));
+        }
+    }
+}
